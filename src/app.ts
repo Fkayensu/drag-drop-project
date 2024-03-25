3bceba4021a6bb53b7597f5f59661fd2aba52d91
Fkayensu <frederickayensuphd@gmail.com>
2024-03-25 11:10:55 -0400

    add css and implement form functionality on class initialization

--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,48 @@
-// Code goes here!
+class ProjectInput {
+  templateElement: HTMLTemplateElement;
+  hostElement: HTMLDivElement;
+  element: HTMLFormElement;
+  titleInputElement: HTMLInputElement;
+  descriptionInputElement: HTMLInputElement;
+  peopleInputElement: HTMLInputElement;
+
+  constructor() {
+    this.templateElement = <HTMLTemplateElement>(
+      document.getElementById("project-input")!
+    );
+    this.hostElement = <HTMLDivElement>document.getElementById("app")!;
+
+    const importedNode = document.importNode(
+      this.templateElement.content,
+      true
+    );
+    this.element = importedNode.firstElementChild as HTMLFormElement;
+    this.element.id = "user-input";
+    this.titleInputElement = <HTMLInputElement>(
+      this.element.querySelector("#title")!
+    );
+    this.descriptionInputElement = <HTMLInputElement>(
+      this.element.querySelector("#description")!
+    );
+    this.peopleInputElement = <HTMLInputElement>(
+      this.element.querySelector("#people")!
+    );
+    this.configure();
+    this.attach();
+  }
+
+  private submitHandler(e: Event) {
+    e.preventDefault();
+    console.log(this.titleInputElement.value);
+  }
+
+  private configure() {
+    this.element.addEventListener("submit", this.submitHandler.bind(this));
+  }
+
+  private attach() {
+    this.hostElement.insertAdjacentElement("afterbegin", this.element);
+  }
+}
+
+const prjInput = new ProjectInput();
