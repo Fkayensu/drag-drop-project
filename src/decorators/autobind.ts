b99787dc0775e912e9f806ef3efd4377641e22e7
Fkayensu <frederickayensuphd@gmail.com>
2024-03-26 12:24:25 -0400

    refactor using es6+ modules feature

--- src/decorators/autobind.ts
+++ src/decorators/autobind.ts
@@ -1,14 +1,12 @@
-namespace App {
-  // autobind function
-  export function Autobind(_: any, _2: string, descriptor: PropertyDescriptor) {
-    const originalMethod = descriptor.value;
-    const adjustedDescriptor: PropertyDescriptor = {
-      configurable: true,
-      get() {
-        const boundFn = originalMethod.bind(this);
-        return boundFn;
-      },
-    };
-    return adjustedDescriptor;
-  }
+// autobind function
+export function Autobind(_: any, _2: string, descriptor: PropertyDescriptor) {
+  const originalMethod = descriptor.value;
+  const adjustedDescriptor: PropertyDescriptor = {
+    configurable: true,
+    get() {
+      const boundFn = originalMethod.bind(this);
+      return boundFn;
+    },
+  };
+  return adjustedDescriptor;
 }
